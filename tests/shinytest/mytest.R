d64004237faea538555bf5c613f16756464b691e
rohit <rohitkwankhede@gmail.com>
2022-12-01 07:25:17 +0530

    ;;;

--- tests/shinytest/mytest.R
+++ tests/shinytest/mytest.R
@@ -1,8 +1,12 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("mytest")
 
+app$setInputs(size = 9)
+app$setInputs(size = 8)
+app$setInputs(size = 7)
 app$snapshot()
-
-app$setInputs(`examplemodule1-button` = "click")
-app$setInputs(`examplemodule1-button` = "click")
+app$snapshot()
+app$setInputs(size = 16)
+app$snapshot()
+app$setInputs(size = 18)
 app$snapshot()
